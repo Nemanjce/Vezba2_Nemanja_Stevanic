39c7b02d1b335454d4d4770784196c840a0cf648
Neca-posao <nemanjce@yahoo.com>
2018-06-12 10:30:48 +0200

    Step 4 - class Course

--- src/course/Course.java
+++ src/course/Course.java
@@ -14,4 +14,22 @@ public class Course {
     private String name;
     private int numberOfClasses;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumberOfClasses(int numberOfClasses) {
+        this.numberOfClasses = numberOfClasses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfClasses() {
+        return numberOfClasses;
+    }
+
+    
+    
 }
--- src/student/Student.java
+++ src/student/Student.java
@@ -1,40 +1,41 @@
-
 package student;
 
-
 public class Student {
-    
+
     private String firstName;
     private String lastName;
     private int yearOfBirth;
-    
-    public String getFirstName(){
-    return firstName;
+
+    public String getFirstName() {
+        return firstName;
     }
-    
-    public void setFirstName(String firstName){
-    this.firstName = firstName;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
-    
-    public String getLastName(){
-    return lastName;}
-    
-    public void setLastName(String lastName){
-    this.lastName = lastName;
-           
+
+    public String getLastName() {
+        return lastName;
     }
-    
-    public int getYearOfBirth(){
-    return yearOfBirth;
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+
     }
-    public void setYearOfBirth(){
-    this.yearOfBirth = yearOfBirth;
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
     }
-    
-    public void info(){
-        System.out.println("First name : ");
-        System.out.println("Last name : ");
-        System.out.println("Year of birth : ");
-    
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
     }
+
+    public void info() {
+        System.out.println("First name: " + getFirstName());
+        System.out.println("Last name: " + getLastName());
+        System.out.println("Year of birth: " + getYearOfBirth());
+
+    }
+
 }
